928f8c76d62a9c27d42122fb19eb30256e762c06
Jema <sdjemma@gmail.com>
2024-05-30 19:19:18 +0200

    Dockerized the entire app

--- backend/src/controllers/regController.ts
+++ backend/src/controllers/regController.ts
@@ -3,82 +3,109 @@ import type { User } from "@prisma/client";
 import { prisma } from "../utils/db";
 import { Registration } from "@prisma/client";
 import {
-    couldStartTrivia,
-    decodedTextSpanIntersectsWith,
-    isQuestionDotToken,
+  couldStartTrivia,
+  decodedTextSpanIntersectsWith,
+  isQuestionDotToken,
 } from "typescript";
 import { deepStrictEqual } from "assert";
 import { getFips } from "crypto";
 import { setDefaultAutoSelectFamily } from "net";
 
 interface AuthRequest extends Request {
-    user?: User;
+  user?: User;
 }
 
 export const createRegistration = async (req: AuthRequest, res: Response) => {
-    try {
-        if (req.user?.id) {
-            const eventId = req.body.eventId;
-            const newRegistration = await prisma.registration.create({
-                data: {
-                    userId: req.user?.id!,
-                    eventId: eventId,
-                },
-            });
-            res.status(201).json({ message: "Registration created successfully" });
-        } else {
-            res.status(400).json({ message: "Bad request" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+  try {
+    if (req.user?.id) {
+      const event = req.body.event;
+      const existingRegistration = await prisma.registration.findFirst({
+        where: {
+          userId: req.user.id,
+          eventId: event.id,
+        },
+      });
+      if (existingRegistration) {
+        res.status(400).json({ message: "Registration already exists" });
+        return;
+      }
+      if (event.maxAttendees - event.registrations.length <= 0) {
+        res.status(400).json({ message: "Event seats are full" });
+        return;
+      }
+      const newRegistration = await prisma.registration.create({
+        data: {
+          userId: req.user?.id!,
+          eventId: event.id,
+        },
+      });
+      res.status(201).json({ message: "Registration created successfully" });
+    } else {
+      res.status(400).json({ message: "Bad request" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const getRegistrations = async (req: AuthRequest, res: Response) => {
-    try {
-        if (req.user?.id) {
-            const registrations = await prisma.registration.findMany({
-                where: {
-                    userId: req.user.id,
-                },
-            });
-            res.status(200).json(registrations);
-        } else {
-            res.status(400).json({ message: "Bad request" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+  try {
+    if (req.user?.id) {
+      const registrations = await prisma.registration.findMany({
+        where: {
+          userId: req.user.id,
+        },
+        include: {
+          event: true,
+        },
+      });
+      res.status(200).json(registrations);
+    } else {
+      res.status(400).json({ message: "Bad request" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const deleteRegistration = async (req: AuthRequest, res: Response) => {
-    try {
-        if (req.user?.id) {
-            const regId = req.body.regId;
-            await prisma.registration.delete({
-                where: {
-                    id: regId,
-                },
-            });
-            res.status(200).json({ message: "Registration deleted successfully" });
-        } else {
-            res.status(400).json({ message: "Bad request" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+  try {
+    if (req.body.eventId && req.user?.id) {
+      await prisma.registration.delete({
+        where: {
+          eventId_userId: {
+            eventId: req.body.eventId,
+            userId: req.user.id,
+          },
+        },
+      });
+      res.status(200).json({ message: "Registration deleted successfully" });
+    } else if (req.user?.id) {
+      const regId = req.body.regId;
+      await prisma.registration.delete({
+        where: {
+          id: regId,
+        },
+      });
+      res.status(200).json({ message: "Registration deleted successfully" });
+    } else {
+      res.status(400).json({ message: "Bad request" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const getEventRegistrations = async (req: Request, res: Response) => {
-    try {
-        const eventId = req.body.eventId;
-        const registrations = await prisma.registration.findMany({
-            where: {
-                eventId: eventId,
-            },
-        });
-        res.status(200).json(registrations);
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" });
-    }
+  try {
+    const eventId = req.body.eventId;
+    const registrations = await prisma.registration.findMany({
+      where: {
+        eventId: eventId,
+      },
+    });
+    res.status(200).json(registrations);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
 import {
-    createEvent,
-    getEvents,
-    getOrganizerEvents,
-    updateEvent,
-    deleteEvent,
+  createEvent,
+  getEvents,
+  getOrganizerEvents,
+  updateEvent,
+  deleteEvent,
 } from "../controllers/eventsController";
 import { verifyToken, isUser } from "../controllers/authController";
 import { isOrganizer } from "../middleware/authorizationMiddleware";
@@ -14,11 +14,11 @@ export const eventsRouter = express.Router();
 eventsRouter.post("/create", verifyToken, isUser, isOrganizer, createEvent);
 eventsRouter.get("/get", verifyToken, isUser, getEvents);
 eventsRouter.get(
-    "/getByOrganizer",
-    verifyToken,
-    isUser,
-    isOrganizer,
-    getOrganizerEvents
+  "/getByOrganizer",
+  verifyToken,
+  isUser,
+  isOrganizer,
+  getOrganizerEvents
 );
 eventsRouter.put("/update", verifyToken, isUser, isOrganizer, updateEvent);
-eventsRouter.delete("/delete", verifyToken, isUser, isOrganizer, deleteEvent);
+eventsRouter.post("/delete", verifyToken, isUser, isOrganizer, deleteEvent);
