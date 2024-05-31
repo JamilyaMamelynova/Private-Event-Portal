4c2b388fbf2a593367166dba7c2c4731b90c8566
Jema <sdjemma@gmail.com>
2024-05-31 20:40:29 +0200

    finished work

--- backend/src/routes/regRoutes.ts
+++ backend/src/routes/regRoutes.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
 import {
-    createRegistration,
-    getRegistrations,
-    getEventRegistrations,
-    deleteRegistration,
+  createRegistration,
+  getRegistrations,
+  getEventRegistrations,
+  deleteRegistration,
 } from "../controllers/regController";
 import { verifyToken, isUser } from "../controllers/authController";
 import { isOrganizer } from "../middleware/authorizationMiddleware";
@@ -13,10 +13,10 @@ export const regRouter = express.Router();
 regRouter.post("/create", verifyToken, isUser, createRegistration);
 regRouter.get("/get", verifyToken, isUser, getRegistrations);
 regRouter.get(
-    "/getByEvent",
-    verifyToken,
-    isUser,
-    isOrganizer,
-    getEventRegistrations
+  "/getByEvent",
+  verifyToken,
+  isUser,
+  isOrganizer,
+  getEventRegistrations
 );
 regRouter.post("/delete", verifyToken, isUser, deleteRegistration);
